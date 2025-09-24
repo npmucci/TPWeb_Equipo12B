edc95ec010b78b46324bf09d5f68e3972f602340
Natalia Mucci <npmucci@gmail.com>
2025-09-23 21:39:54 -0300

    creacion del proyecto y clases de dominio

---
+++ Dominio/Voucher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Voucher
+    {
+        private string Codigo { get; set; }
+        private Cliente Cliente { get; set; }
+        private DateTime Fecha { get; set; }
+        private Articulo Articulo { get; set; }
+    }
+}
+
